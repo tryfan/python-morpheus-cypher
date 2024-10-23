d3b1b201d5787a751cf63e907f3c00c6f61aee8e
Tyler Boyd <tyboyd02@gmail.com>
2024-10-23 08:56:20 -0400

    Use executionLeaseToken over apiAccessToken if available

--- morpheuscypher/__init__.py
+++ morpheuscypher/__init__.py
@@ -78,10 +78,10 @@ class Cypher:
             self.url = self.url.rstrip('/')
 
         if token is None:
-            if "morpheus_token" in os.environ:
-                self.token = os.environ.get("morpheus_token", None)
-            elif morpheus is not None:
-                self.token = morpheus['morpheus']['apiAccessToken']
+            self.token = os.environ.get("morpheus_token", None)
+
+            if not self.token and morpheus:
+                self.token = morpheus.get("executionLeaseToken", None) or morpheus['morpheus']['apiAccessToken']                   
         else:
             self.token = token
         if self.token is None:
